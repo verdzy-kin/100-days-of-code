c04e32278ef4ee303d0edcf39f5d9cb822cae537
Delsy Kinyuy <delsykinyuy6@gmail.com>
2024-12-10 14:49:53 +0100

    mine

--- Day 2/exercise 2.1.py
+++ Day 2/exercise 2.1.py
@@ -1,4 +1,4 @@
 two_digit_num = input("Enter 2 digit number: ")
-sum = int(two_digit_num[0]) + int(two_digit_num[1])
-print(type(sum))
-print(sum)
+s = int(two_digit_num[0]) + int(two_digit_num[1])
+print(type(s))
+print(s)
